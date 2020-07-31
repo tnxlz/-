5808daaecb583dafead3480e3825654922379f01
tangna <2375720914@qq.com>
2020-07-31 11:55:13 +0800

    列表渲染

---
+++ 小程序/pages/for/for.js
@@ -0,0 +1,67 @@
+// pages/for/for.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    movies:['星际穿越','盗梦空间','哈利波特'],
+    num:[['1123','132132','32321'],['654','524'],['555','6563','jyg']]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
